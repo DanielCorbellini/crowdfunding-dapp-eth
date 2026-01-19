9ccdbcc42966e3fd7031079563ef18aa10d54245
Daniel <daniel.corbellini28@gmail.com>
2026-01-19 16:42:21 -0300

    feat: add a CampaignCreate event for CampaignFactory

--- contracts/CampaignFactory.sol
+++ contracts/CampaignFactory.sol
@@ -7,9 +7,13 @@ import "./Campaign.sol";
 contract CampaignFactory {
     Campaign[] public deployedCampaigns;
 
+    event CampaignCreated(address indexed campaign, address indexed creator);
+
     function createCampaign(uint minimumContribution) public {
         Campaign newCampaign = new Campaign(minimumContribution, msg.sender);
         deployedCampaigns.push(newCampaign);
+
+        emit CampaignCreated(address(newCampaign), msg.sender);
     }
 
     function getDeployedCampaigns() public view returns (Campaign[] memory) {
